4c4e2c1c75f37f828b350af5869e62752d55f2e7
HetulKanani <86960931+HetulKanani@users.noreply.github.com>
2022-03-28 17:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mineflayer = require('mineflayer')
 const cmd = require('mineflayer-cmd').plugin
 const fs = require('fs');
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const GoalFollow = goals.GoalFollow
+const GoalBlock = goals.GoalBlock
 let rawdata = fs.readFileSync('config.json');
 let data = JSON.parse(rawdata);
 var lasttime = -1;
@@ -71,4 +74,48 @@ bot.on('spawn',function() {
 bot.on('death',function() {
     bot.emit("respawn")
 });
+bot.loadPlugin(pathfinder)
+
+function followPlayer() {
+    const playerCI = bot.players['TheDudeFromCI']
+
+    if (!playerCI || !playerCI.entity) {
+        bot.chat("I can't see CI!")
+        return
+    }
+
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = []
+
+    bot.pathfinder.setMovements(movements)
+
+    const goal = new GoalFollow(playerCI.entity, 1)
+    bot.pathfinder.setGoal(goal, true)
+}
+
+function locateEmeraldBlock () {
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = []
+    bot.pathfinder.setMovements(movements)
+
+    const emeraldBlock = bot.findBlock({
+        matching: mcData.blocksByName.emerald_block.id,
+        maxDistance: 32
+    })
+
+    if (!emeraldBlock) {
+        bot.chat("I can't see any emerald blocks!")
+        return
+    }
+
+    const x = emeraldBlock.position.x
+    const y = emeraldBlock.position.y + 1
+    const z = emeraldBlock.position.z
+    const goal = new GoalBlock(x, y, z)
+    bot.pathfinder.setGoal(goal)
+}
+
+bot.once('spawn', locateEmeraldBlock)
 
